871b01d746e289a8b5056fa389be45ed0a5bda67
extreme4all <40169115+extreme4all@users.noreply.github.com>
2019-08-28 16:02:29 +0200

    Update playground copy.ps1

--- playground copy.ps1
+++ playground copy.ps1
@@ -5,29 +5,69 @@ $runspaces = @()
  
 $scriptblock = {
     Param (
-        [string]$server,
-        [int]$count,
-        [string]$scriptblock,
-        $pool,
-        $go
+        $server,
+        $count,
+        $go,
+        $scriptblock
     )
     $scriptblock = ([scriptblock]::Create($scriptblock))
     # Pretend I connected to a server here and gathered some info
     Write-Output "Type: $server Loop: $count isFirst:$go"
+
+    # makes ar runspace
+    function get-runspace{
+        param(
+            $server,
+            $count,
+            $go
+        )
+        
+        $runspace = [PowerShell]::Create()
+        $null = $runspace.AddScript($scriptblock)
+        $null = $runspace.AddArgument($server)
+        $null = $runspace.AddArgument($count)
+        $null = $runspace.AddArgument($go)
+        $null = $runspace.AddArgument($scriptblock)
+        $runspace.RunspacePool = $pool
+        $runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
+        # retrieve results
+
+        while ($runspaces.Status -ne $null){
+            $completed = $runspaces | Where-Object { $_.Status.IsCompleted -eq $true }
+            foreach ($runspace in $completed){
+                $results = $runspace.Pipe.EndInvoke($runspace.Status)
+                # write output to get the data in the main loop
+                foreach($result in $results){Write-Output $result}
+                $runspace.Status = $null
+            }
+        }
+    }
+    # launch nested runspaces
     if($go -eq 1){
         1..5 | ForEach-Object {
             $go = 0
-            $runspace = [PowerShell]::Create()
-            $null = $runspace.AddScript($scriptblock)
-            $null = $runspace.AddArgument("Nest")
-            $null = $runspace.AddArgument($count)
-            $null = $runspace.AddArgument($scriptblock)
-            $null = $runspace.AddArgument($pool)
-            $null = $runspace.AddArgument($go)
-            $runspace.RunspacePool = $pool
-            $runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
+            $count = ++$i
+            get-runspace "Nested" $count $go
         }
     }
+}
+# makes ar runspace
+function get-runspace{
+    param(
+        $server,
+        $count,
+        $go,
+        $scriptblock
+    )
+    
+    $runspace = [PowerShell]::Create()
+    $null = $runspace.AddScript($scriptblock)
+    $null = $runspace.AddArgument($server)
+    $null = $runspace.AddArgument($count)
+    $null = $runspace.AddArgument($go)
+    $null = $runspace.AddArgument($scriptblock)
+    $runspace.RunspacePool = $pool
+    $runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
     while ($runspaces.Status -ne $null){
         $completed = $runspaces | Where-Object { $_.Status.IsCompleted -eq $true }
         foreach ($runspace in $completed){
@@ -35,31 +75,13 @@ $scriptblock = {
             $runspace.Status = $null
         }
     }
-    
 }
- 
+
 1..10 | ForEach-Object {
     $go = 1
-    $runspace = [PowerShell]::Create()
-    $null = $runspace.AddScript($scriptblock)
-    $null = $runspace.AddArgument("Original")
-    $null = $runspace.AddArgument(++$i)
-    $null = $runspace.AddArgument($scriptblock)
-    $null = $runspace.AddArgument($pool)
-    $null = $runspace.AddArgument($go)
-    $runspace.RunspacePool = $pool
-    $runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
+    $count = ++$i
+    get-runspace "Original" $count $go $scriptblock
     
-}
- 
-while ($runspaces.Status -ne $null)
-{
-    $completed = $runspaces | Where-Object { $_.Status.IsCompleted -eq $true }
-    foreach ($runspace in $completed)
-    {
-        $runspace.Pipe.EndInvoke($runspace.Status)
-        $runspace.Status = $null
-    }
 }
 $pool.Close()
 $pool.Dispose()
